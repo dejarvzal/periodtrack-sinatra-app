e1b0b5219088dbce8f1a23566af66dcdcedfecf9
dejarvzal <diannejarvisj@gmail.com>
2020-06-07 18:34:33 -0700

    correct syntax errors in UsersController and login view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,12 @@ class UsersController < ApplicationController
     post "/login" do
         user = User.find_by(email: params[:email])
         # binding.pry
-        if user && user.authenticate(params[:password])
-            #log them in 
-         session[:user_id] = user.id 
-        #  binding.pry
-            #redirect to profile
-            redirect "/users/#{user.id}"
-        else
-            #show error message
-            redirect '/login'
+        if user && user.authenticate(params[:password]) #log them in 
+            # binding.pry
+        session[:user_id] = user.id  #redirect to profile
+        redirect "/users/#{user.id}"
+        else #show error message
+           redirect '/login'
         end
     end
 

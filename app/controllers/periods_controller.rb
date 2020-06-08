fd08dd955a4f057b9384be814acd5711b4d4562b
dejarvzal <diannejarvisj@gmail.com>
2020-06-07 22:28:02 -0700

    route and show page for single period

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -7,9 +7,14 @@ get '/periods' do
    erb :"periods/index"
 end
 
+get '/periods/:id' do
+    @period = Period.find_by(id: params[:id])
+    erb :"/periods/show"
+end
+
 #CREATE
 post '/periods' do
-    
+
 end
 
 #UPDATE

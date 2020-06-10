85a65403e3f468d0b2ed2ed6b913e4adf8c2e7c4
dejarvzal <diannejarvisj@gmail.com>
2020-06-09 19:23:49 -0700

    fixing issues with saving user and period objects

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -1,37 +1,35 @@
 class PeriodsController < ApplicationController
 
+  
 
-#READ 
-get '/periods' do
-   @periods = Period.all
-   erb :"periods/index"
-end
+    #READ 
+    get '/periods' do
+    @periods = Period.all
+    erb :"periods/index"
+    end
 
-#CREATE
+    #CREATE
+    get '/periods/new' do
+        erb :"/periods/new"
+    end
 
-get '/periods/new' do
-    erb :"/periods/new"
-end
+    post '/periods/new' do
+        @period = Period.create(month: params[:month], start_day: params[:start_day], note: params[:note], user_id: current_user.id)
+        redirect "/periods/#{@period.id}"
+    
+    end
 
-post '/periods/new' do
-    period = Period.create(month: params[:month], start_day: params[:start_day], note: params[:note], user_id: current_user.id)
-    #receives input data from user, create the user, logs them in.
-    redirect "/periods/#{period.id}"
-   
-end
+    get '/periods/:id' do
+        @period = Period.find_by(id: params[:id])
+        erb :"/periods/show"
+    end
 
-get '/periods/:id' do
-    @period = Period.find_by(id: params[:id])
-    erb :"/periods/show"
-end
+    #UPDATE
+    #edit button that takes to the form to get the id
+    #render an edit form
+    #patch metod/route that will update the post
 
-
-
-
-#UPDATE
-
-
-#DELETE
+    #DELETE
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,21 +14,24 @@ class UsersController < ApplicationController
         end
     end
 
-    get "/users/:id" do
-        erb :"/users/show"
-    end
-
     get '/signup' do
         erb :"/users/signup"
     end
    
    post '/users' do
         # user = User.create(params)
+        binding.pry
         user = User.create(name: params[:name], email: params[:email], password: params[:password])
         session[:user_id] = user.id
         redirect "/users/#{user.id}"
    end
-   
+
+    get "/users/:id" do
+        @user = User.find_by(id: params[:id])
+        erb :"/users/show"
+    end
+
+     
    get '/logout' do
     session.clear
     redirect '/'

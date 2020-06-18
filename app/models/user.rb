4e76de26d41377f20ff426fd87cab36ec8c67d98
dejarvzal <diannejarvisj@gmail.com>
2020-06-18 09:40:58 -0700

    edit show page to show only current user's period

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_many :periods
     has_secure_password
 
-    validates :name, :email, :password, presence: true
+    validates :name, :email, presence: true
     validates :email, uniqueness: true
   
 end
